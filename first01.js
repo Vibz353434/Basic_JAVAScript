23c9ecb34b3cf918489d4b5e3cc02b4b03439cc4
vaibhav <vaibhavpatil353434@example.com>
2023-08-20 13:26:41 +0530

    first commit

---
+++ first01.js
@@ -0,0 +1,18 @@
+// const names = document.getElementsByClassName('vp')
+// console.log("names")
+// var n = 2;
+// function square(num) {
+//     var ans = num * num;
+//     return ans;
+// }
+// var square2 = square(n);
+// var square4 = square(4);
+
+
+getName();
+console.log(x);
+var x = 7;
+
+function getName() {
+    console.log("Its just beggining");
+}
