ebfcb4bbc807cebc64f95d55cfb01bbc700b54a9
vaibhav <vaibhavpatil353434@example.com>
2023-08-23 01:25:20 +0530

    first commit

--- first01.js
+++ first01.js
@@ -15,17 +15,19 @@
 //     console.log("Its just beggining");
 // }
 
-var x = 1;
-a();
-b();
-console.log(x);
+// var x = 1;
+// a();
+// b();
+// console.log(x);
+// function a() {
+//     var x = 10;
+//     console.log(x);
+// }
+// function b() {
+//     var x = 100;
+//     console.log(x);
+// }
 
-function a() {
-    var x = 10;
-    console.log(x);
-}
-
-function b() {
-    var x = 100;
-    console.log(x);
-}
+let a = 10;
+console.log(a);
+var b = 100;
